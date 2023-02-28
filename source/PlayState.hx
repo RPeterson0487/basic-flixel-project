882af0f1c320f6f9974bcf72dec5b5ade5566e7c
Richard Peterson <peterson4487@live.missouristate.edu>
2023-02-28 15:59:09 -0600

    I borked it... Push to save progress.

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,5 +1,6 @@
 package;
 
+import flixel.math.FlxPoint;
 import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.FlxG;
 import flixel.FlxState;
@@ -19,6 +20,8 @@ class PlayState extends FlxState
 	static var NUMSSHEROMAX:Int = 20;
 	static var SSHXPOS:Float = 320;
 	static var SSHYPOS:Float = 180;
+	static var BOXSIZE = .125; // This is a percentage of the play screen
+	static var WALLWIDTH = 5;
 
 	var controllableHero = new ControllableHero(CHXPOS, CHYPOS);
 	var screenSaverHeroGroup = new FlxTypedGroup<ScreenSaverHero>();
@@ -27,6 +30,8 @@ class PlayState extends FlxState
 	override public function create()
 	{
 		super.create();
+
+		// bgColor = 0xFFFFFFFF;
 
 		var myHero = new Hero(HEROXPOS, HEROYPOS);
 		add(myHero);
@@ -44,20 +49,23 @@ class PlayState extends FlxState
 			add(screenSaverHero);
 		}
 
+		boxWall();
+		add(wallGroup);
+
 		add(controllableHero);
 
-		var wallTop = new Wall(80, 60, 480, 5);
-		var wallBottom = new Wall(80, 420, 480, 5);
-		var wallLeft = new Wall(80, 60, 5, 360);
-		var wallRight = new Wall(560, 60, 5, 365);
-		wallGroup.add(wallTop);
-		wallGroup.add(wallBottom);
-		wallGroup.add(wallLeft);
-		wallGroup.add(wallRight);
-		add(wallTop);
-		add(wallBottom);
-		add(wallLeft);
-		add(wallRight);
+		/* var wallTop = new Wall(80, 60, 480, 5);
+			var wallBottom = new Wall(80, 420, 480, 5);
+			var wallLeft = new Wall(80, 60, 5, 360);
+			var wallRight = new Wall(560, 60, 5, 365); */
+		/* wallGroup.add(wallTop);
+			wallGroup.add(wallBottom);
+			wallGroup.add(wallLeft);
+			wallGroup.add(wallRight); */
+		/* add(wallTop);
+			add(wallBottom);
+			add(wallLeft);
+			add(wallRight); */
 	}
 
 	override public function update(elapsed:Float)
@@ -66,6 +74,24 @@ class PlayState extends FlxState
 		FlxG.overlap(controllableHero, screenSaverHeroGroup, notifyCollision);
 
 		super.update(elapsed);
+	}
+
+	function boxWall()
+	{
+		var topCornerX = FlxG.width * BOXSIZE;
+		var topCornerY = FlxG.height * BOXSIZE;
+		var bottomCornerX = topCornerX * (1 - BOXSIZE);
+		var bottomCornerY = topCornerY * (1 - BOXSIZE);
+
+		var wallTop = new Wall(topCornerX, topCornerY, (Std.int(bottomCornerX - topCornerX)), WALLWIDTH);
+		var wallBottom = new Wall(topCornerX, bottomCornerY, (Std.int(bottomCornerY - bottomCornerX)), WALLWIDTH);
+		var wallLeft = new Wall(topCornerX, topCornerY, WALLWIDTH, (Std.int(bottomCornerY - topCornerY)));
+		var wallRight = new Wall(topCornerX, bottomCornerY, WALLWIDTH, (Std.int(bottomCornerY - topCornerY) + 5));
+
+		wallGroup.add(wallTop);
+		wallGroup.add(wallBottom);
+		wallGroup.add(wallLeft);
+		wallGroup.add(wallRight);
 	}
 
 	function notifyCollision(controllableHero, screenSaverHero)
--- source/Wall.hx
+++ source/Wall.hx
@@ -11,14 +11,14 @@ class Wall extends FlxSprite
 		Creates an individual wall.
 		@param	xPos		The x coordinate where the wall will start.
 		@param	yPos		The y coordinate where the wall will start.
-		@param	wallWidth	The size of the wall in the x direction.
-		@param	wallHeight	The size of the wall in the y direction.
+		@param	wallSizeX	The size of the wall in the x direction.
+		@param	wallsizeY	The size of the wall in the y direction.
 	**/
-	public function new(xPos:Float = 0, yPos:Float = 0, wallWidth:Int = 0, wallHeight:Int = 0)
+	public function new(xPos:Float = 0, yPos:Float = 0, wallSizeX:Int = 0, wallsizeY:Int = 0)
 	{
 		super(xPos, yPos);
 
-		makeGraphic(wallWidth, wallHeight);
+		makeGraphic(wallSizeX, wallsizeY);
 
 		immovable = true;
 	}
